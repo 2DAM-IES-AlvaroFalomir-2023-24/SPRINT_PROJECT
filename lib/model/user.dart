c1f21b3e25f140ce90eff08e0a2db243b2f4a4bc
Alejandro Nebot <alneflo27@gmail.com>
2024-02-13 19:52:36 +0100

    Updated user

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,38 +1,36 @@
-import 'package:intl/intl.dart';
 import 'package:sprint/model/language.dart';
 
-import 'gender.dart';
-import 'marital_status.dart';
-
 class User{
-  const User(this.login, this.password, this.active, this.name, this.email, this.lang, [this.id]);
+  const User(this.email, this.password, this.active, this.name, this.lang, [this.id, this.avatar = ""]);
   final int? id;
-  final String login;
+  final String email;
   final String password;
   final bool active;
   final String name;
-  final String email;
+  final String avatar;
   final Language lang;
 
   static User fromJson(dynamic json){
     return User(
-      json['login'].toString(),
+      json['email'].toString(),
       json['password'].toString(),
       json['active'] as bool,
       json['name'].toString(),
-      json['email'].toString(),
       Language.setLanguageByString(json['lang'].toString()),
+      json['id'] as int,
+      json['avatar'].toString(),
     );
   }
 
   Map toJson(){
     return {
-      'login': login,
+      'login': email,
       'password': password,
       'active': active,
       'name': name,
       'email': email,
-      'lang': lang.name
+      'lang': lang.name,
+      'avatar': avatar
     };
   }
 
