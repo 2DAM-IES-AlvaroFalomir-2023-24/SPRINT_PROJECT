9d8bb1cc8eaae04a2cfc7f7dc923d2c0fc853270
Alejandro Nebot <alneflo27@gmail.com>
2024-02-13 19:40:52 +0100

    CRUD methods created

--- lib/data/odoo_connect.dart
+++ lib/data/odoo_connect.dart
@@ -1,3 +1,4 @@
+import 'package:get/get.dart';
 import 'package:logger/logger.dart';
 import 'package:sprint/odoo-rpc/odoo_client.dart';
 
@@ -14,34 +15,95 @@ class OdooConnect{
 
   static const odooServerURL = SPRINT_CONNECTION;
   static final client = OdooClient(odooServerURL);
+
+  static final logger = Logger();
   
   static void initialize() async{
     await client.authenticate(SPRINT_DATABASE, SPRINT_USER, SPRINT_PASSWORD);
   }
 
-  /*static Future<User> getUserByEmail() async{
-    var logger = Logger();
+  static Future<List<User>> getUsers() async{
+    List<User> found = [];
     try {
       List res = await client.callKw({
-        'model': 'res.users',
-        'method': 'search_read',
-        'args': [],
+        'model': 'res.users', 'method': 'search_read', 'args': [],
         //Selección de campos a mostrar
         'kwargs': {
-          "domain": [["login", "=", "damproyectoflutter@gmail.com"]],
-          "fields":["id", "login", "password", "active", "name", "email", "lang", "tz"]
+          //Selección de campos en la query
+          "fields":["id", "login", "password", "active", "name", "email", "lang"]
         }
       });
       //Obtención de resultados en formato JSON
       if (res.isNotEmpty) {
-        for (var result in res) {
+        for(var result in res){
           var temp = convert.jsonEncode(result);
-          logger.i(convert.jsonDecode(temp));
+          found.add(User.fromJson(convert.jsonDecode(temp)));
         }
       }
+    }catch(a){logger.e(a);}
+    return found;
+  }
+
+  static Future<User?> getUserByEmail(String email) async{
+    User? found;
+    try {
+      if(email.isEmpty){
+        throw Exception("Email cannot be empty");
+      }
+      List res = await client.callKw({
+        'model': 'res.users', 'method': 'search_read', 'args': [],
+        //Selección de campos a mostrar
+        'kwargs': {
+          //Filtrado por login
+          "domain": [["login", "=", email]],
+          //Selección de campos en la query
+          "fields":["id", "login", "password", "active", "name", "email", "lang"]
+        }
+      });
+      //Obtención de resultados en formato JSON
+      if (res.isNotEmpty) {
+        var temp = convert.jsonEncode(res[0]);
+        found = User.fromJson(convert.jsonDecode(temp));
+      }
+    }catch(a){logger.e(a);}
+    return found;
+  }
+
+  static Future<bool> modifyUser(User user) async{
+    try {
+      if(user.id == null){
+        throw Exception("User id cannot be empty");
+      }
+      await client.callKw({
+        'model': 'res.users', 'method': 'write', 'args': [
+          user.id,
+          user.toJson()
+        ],
+        'kwargs': {}
+      });
+      return true;
     }catch(a){
-      print(a);
+      logger.e(a);
+      return false;
     }
-  }*/
+  }
+
+  static Future<bool> createUser(User user) async{
+    try {
+      if(user.id != null){
+        throw Exception("User id cannot be created");
+      }
+      await client.callKw({
+        'model': 'res.users', 'method': 'create', 'args': [
+          user.toJson()
+        ],
+        'kwargs': {}
+      });
+      return true;
+    }catch(a){
+      logger.e(a);
+      return false;
+    }
+  }
   
 }
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,93 +1,39 @@
 import 'package:intl/intl.dart';
+import 'package:sprint/model/language.dart';
 
 import 'gender.dart';
 import 'marital_status.dart';
 
 class User{
-  User(
-      this.name, this.privateEmail,
-      this.gender, this.birthday, [this.id, this.active = true, this.barcode, this.workEmail = "", this.phone = "",
-        this.marital = MaritalStatus.single, this.drivingLicense = false]);
+  const User(this.login, this.password, this.active, this.name, this.email, this.lang, [this.id]);
   final int? id;
-  final String name;
-  final String privateEmail;
+  final String login;
+  final String password;
   final bool active;
-  final String? barcode;
-  final Gender gender;
-  final DateTime? birthday;
-  final String workEmail;
-  final String phone;
-  final MaritalStatus marital;
-  final bool drivingLicense;
+  final String name;
+  final String email;
+  final Language lang;
 
-  Map toJson(){
-    return {
-      'id': id,
-      'name': name,
-      'private_email': privateEmail,
-      'active': active,
-      'barcode': barcode,
-      'gender': gender.name,
-      'birthday': getBirthDate(),
-      'work_email': workEmail,
-      'phone': phone,
-      'marital': marital.name,
-      'driving_license': drivingLicense
-    };
-  }
-
-  Map toPartJson(){
-    return {
-      'name': name,
-      'private_email': privateEmail,
-      'active': active,
-      'barcode': barcode,
-      'gender': gender.name,
-      'birthday': getBirthDate(),
-      'work_email': workEmail,
-      'phone': phone,
-      'marital': marital.name,
-      'driving_license': drivingLicense
-    };
-  }
-
-  static User fromJson(Map<dynamic, dynamic> json){
+  static User fromJson(dynamic json){
     return User(
-        json['name'].toString(),
-        json['private_email'].toString(),
-        Gender.setGenderByString(json['gender'].toString()),
-        json['birthday'].toString() == "false" ? null : DateTime.parse(json['birthday'].toString()),
-        json['id'] as int,
-        json['active'] as bool,
-        json['barcode'] == Null ? "" : json['barcode'].toString(),
-        json['work_email'].toString(),
-        json['phone'].toString(),
-        MaritalStatus.setMaritalByString(json['marital'].toString()),
-        json['driving_license'] as bool
+      json['login'].toString(),
+      json['password'].toString(),
+      json['active'] as bool,
+      json['name'].toString(),
+      json['email'].toString(),
+      Language.setLanguageByString(json['lang'].toString()),
     );
   }
 
-  String getBirthDate(){
-    if(birthday != null){
-      var numFormat = NumberFormat("00", "es_ES");
-      return "${birthday!.year}-${numFormat.format(birthday!.month)}-${numFormat.format(birthday!.day)}";
-    }else{
-      return "null";
-    }
+  Map toJson(){
+    return {
+      'login': login,
+      'password': password,
+      'active': active,
+      'name': name,
+      'email': email,
+      'lang': lang.name
+    };
   }
 
-  @override
-  String toString() {
-    return "{\nid: $id,\n"
-        "name: $name,\n"
-        "private_email: $privateEmail\n"
-        "active: $active\n"
-        "barcode: $barcode\n"
-        "gender: ${gender.name}\n"
-        "birthday: ${getBirthDate()}\n"
-        "work_email: $workEmail\n"
-        "phone: $phone\n"
-        "marital: ${marital.name}\n"
-        "driving_license: $drivingLicense\n}\n";
-  }
 }
