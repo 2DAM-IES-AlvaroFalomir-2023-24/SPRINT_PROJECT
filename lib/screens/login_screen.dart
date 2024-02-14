ac28ba306d1f3d1141a469af1541d2dc312759b6
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2024-02-14 21:17:28 +0100

    Agrega pantalla de registro y inicio

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sprint/data/odoo_connect.dart';
+import 'package:sprint/screens/register_screen.dart';
 
 class LoginScreen extends StatefulWidget{
   const LoginScreen({super.key});
@@ -21,7 +22,71 @@ class LoginScreenState extends State<LoginScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //body: Image.memory(base64Decode(a.avatar)),
+      appBar: AppBar(
+        title: const Text('login'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Introduce tu nombre de usuario',
+                  labelText: 'Nombre de usuario',
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.lock),
+                  hintText: 'Introduce tu contraseña',
+                  labelText: 'Contraseña',
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Aquí va la lógica de inicio de sesión
+                  },
+                  child: const Text('Iniciar sesión'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterScreen()));
+                  },
+                  child: const Text('Registrarse'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Aquí va la lógica de inicio de sesión
+                  },
+                  child: const Text('Home'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Aquí va la lógica de inicio de sesión
+                  },
+                  child: const Text('User Profile'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
