0a018dbea2694ed43964ce6b59200e17236e337c
Tony Ureña <105971074+TonyUrena@users.noreply.github.com>
2024-02-14 15:55:51 +0100

    Layout base para gestión de usuario

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sprint/data/odoo_connect.dart';
 import 'package:sprint/screens/login_screen.dart';
+import 'package:sprint/screens/user_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sprint Project',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: SafeArea(
-        child: LoginScreen(),
-      )
-    );
+        title: 'Sprint Project',
+        debugShowCheckedModeBanner: false,
+        // Tema generado automáticamente utilizando el color corporativo de Odoo como
+        // semilla para el esquema de color
+        // TODO falta añadir un tono rojizo para los botones que tengan opciones peligrosas
+        theme:ThemeData(
+            useMaterial3: true,
+            colorSchemeSeed: const Color(0xFF714B67)),
+        darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            useMaterial3: true,
+            colorSchemeSeed: const Color(0xFF714B67)),
+        home: SafeArea(
+          child: UserScreen(),
+        ));
   }
 }
