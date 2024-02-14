175d7b8e0e8ae5cb95e034552d9f513a53706166
JulenR14 <julen20191414@gmail.com>
2024-02-14 19:08:20 +0100

    He implentado hasta el inisState

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,11 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget{
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen>{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Usuario que utilizaremos para manejar el registro del usuario
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    //Configuramos un oyente para el objeto que maneja el registro de usuario
+    _auth.authStateChanges().listen((user) {
+      setState(() {
+        //Si el usuario es diferente de null, lo asignamos a la variable _user
+        _user = user;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold();
   }
-  
 }
