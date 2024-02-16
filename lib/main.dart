c86d3bf822bdbe97088e5d759329ad1f72abea31
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2024-02-17 00:40:21 +0100

    He cambiado una libreria que estaba mal importada y he aplicado el ChangeNotifierProvider para que funcione el registro con google.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sprint/bloc/google_sign_in.dart';
 import 'package:sprint/screens/verify_auth_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:sprint/data/odoo_connect.dart';
@@ -12,7 +13,6 @@ import 'package:sprint/app_localizations.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:sprint/firebase_options.dart';
-import 'bloc/google_sign_in.dart';
 
 Future main() async {
   await dotenv.load(fileName: "././assets/.env");
@@ -33,37 +33,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<UserBloc>(
         create: (context) => UserBloc(),
-        child: MaterialApp(
-            title: 'Sprint Project',
-            debugShowCheckedModeBanner: false,
-            supportedLocales: const [
-              Locale('en', 'US'),
-              Locale('es', 'ES'),
-            ],
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            localeResolutionCallback: (locale, supportedLocales) {
-              for (var supportedLocale in supportedLocales) {
-                if (supportedLocale.languageCode == locale?.languageCode &&
-                    supportedLocale.countryCode == locale?.countryCode) {
-                  return supportedLocale;
+        child: ChangeNotifierProvider(
+          create: (context) => GoogleSignInProvider(),
+          child: MaterialApp(
+              title: 'Sprint Project',
+              debugShowCheckedModeBanner: false,
+              supportedLocales: const [
+                Locale('en', 'US'),
+                Locale('es', 'ES'),
+              ],
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              localeResolutionCallback: (locale, supportedLocales) {
+                for (var supportedLocale in supportedLocales) {
+                  if (supportedLocale.languageCode == locale?.languageCode &&
+                      supportedLocale.countryCode == locale?.countryCode) {
+                    return supportedLocale;
+                  }
                 }
-              }
-              return supportedLocales.first;
-            },
-            theme: ThemeData(
-                useMaterial3: true, colorSchemeSeed: const Color(0xFF714B67)),
-            darkTheme: ThemeData(
-                useMaterial3: true,
-                brightness: Brightness.dark,
-                colorSchemeSeed: const Color(0xFF714B67)),
-            home: const SafeArea(
-                //Comentar la clase VerifyAuthBloc del archivo verify_auth.dart
-                // para hacer las pruebas en local
-                // y escribe la clase de tu pantalla
-                child: VerifyAuthBloc())));
+                return supportedLocales.first;
+              },
+              theme: ThemeData(
+                  useMaterial3: true, colorSchemeSeed: const Color(0xFF714B67)),
+              darkTheme: ThemeData(
+                  useMaterial3: true,
+                  brightness: Brightness.dark,
+                  colorSchemeSeed: const Color(0xFF714B67)),
+              home: const SafeArea(
+                  //Comentar la clase VerifyAuthBloc del archivo verify_auth.dart
+                  // para hacer las pruebas en local
+                  // y escribe la clase de tu pantalla
+                  child: VerifyAuthBloc())),
+        ));
   }
 }
